00354e7e6934d6329967a9074ef02fe26516de05
0xMGuerra <mguerrargz@gmail.com>
2024-03-08 13:09:55 +0000

    Inserted navbar into the other pages. Added js function to load specific content.

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,23 @@
 function include(tag, file) {
     $(tag).load(file);
 }
+function includeByQuery(tag, query, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(html => {
+            const parser = new DOMParser();
+            const parsedHTML = parser.parseFromString(html, 'text/html');
+            const content = parsedHTML.querySelector('#' + query);
+            template.appendChild(content);
+        })
+        .catch(error => console.error('Error al cargar el archivo: ', error));
+
+    function loadCSS() {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = file.replace('.html', '.css'); // Suponiendo que el CSS tiene el mismo nombre que la página HTML
+        document.head.appendChild(link);
+    }
+    loadCSS();
+}
