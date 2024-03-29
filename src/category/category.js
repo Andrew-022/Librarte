dcdfaa727be6e67498cebd9651ba8e569a125fdf
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2024-03-29 21:47:11 +0000

    category dropdown

--- src/category/category.js
+++ src/category/category.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
                 bookCover.classList.add('bookCover');
                 bookCover.src = libro.cover;
                 bookCover.alt = libro.title;
-                
+
                 const bookTitle = document.createElement('b');
                 bookTitle.classList.add('bookTitle');
                 bookTitle.textContent = libro.title;
@@ -36,3 +36,31 @@ $(document).ready(function() {
         })
         .catch(error => console.error('Error al cargar el JSON de libros:', error));
 });
+
+const sorts=["Mas valorados", "Titulo A-Z", "Titulo Z-A", "Precio ascendente", "Precio descendente"]
+
+
+function changeSort(id) {
+    var sort = document.getElementById("selectedSort");
+    sort.textContent = sorts[id];
+}
+
+
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+}
+// Cierra el menú desplegable si el usuario hace clic fuera de él
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
