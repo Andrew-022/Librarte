71025ec755bbe1dd048b28cc3a910d8271b8d015
sheilacazrod <145383241+sheilacazrod@users.noreply.github.com>
2024-04-09 13:39:28 +0100

    search working with json

--- Angular/src/app/search/search.component.ts
+++ Angular/src/app/search/search.component.ts
@@ -1,11 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import { DatabaseJSONService } from "../services/database-json.service";
 import { BookResponse } from "../model/book";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
