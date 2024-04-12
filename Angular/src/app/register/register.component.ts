646099aa1c8e66afe4024f6bbfe31fafc88a2763
sheilacazrod <145383241+sheilacazrod@users.noreply.github.com>
2024-04-12 15:26:25 +0100

    registerform and user.json

--- Angular/src/app/register/register.component.ts
+++ Angular/src/app/register/register.component.ts
@@ -1,27 +1,29 @@
 import { Component } from '@angular/core';
-import {ReactiveFormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {validateWorkspace} from "@angular/cli/src/utilities/config";
+import {ReactiveFormsModule, FormBuilder, Validators} from '@angular/forms';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  resultado: string = "";
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
   }
-  registerform = this.fb.group({
+  registerForm = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
-    apellidos: ['',[ Validators.required]],
-    mail: ['', [Validators.required, Validators.email]],
+    apellidos: ['',[ Validators.required,Validators.pattern('[a-zA-Z ]*')]],
+    mail: ['', [Validators.required, Validators.email, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
     contraseña: ['', [Validators.required,Validators.maxLength(16),Validators.minLength(8)]]
   })
   submit(){
-    if (this.registerform.valid)
-      this.resultado = "Registro con exito"
-    else
-      this.resultado = "Hay datos inválidos"
+    if(this.registerForm.valid) {
+      const formData = this.registerForm.value;
+    this.http.post('data/users.json', JSON.stringify(formData)).subscribe({
+        next: (response) => console.log('Respuesta del servidor:', response),
+        error: (error) => console.error('Error al enviar datos al servidor', error),
+      })
+    }
   }
 }
