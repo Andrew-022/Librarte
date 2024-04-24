b1a68d7964cebe3dadd3e40b8dc61e8f87e80e4f
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2024-04-24 22:02:40 +0100

    firebase getAutorById and getBookbyId

--- Angular/src/app/author-details/author-details.component.ts
+++ Angular/src/app/author-details/author-details.component.ts
@@ -3,7 +3,6 @@ import {author} from "../model/author";
 import {BookComponent} from "../book/book.component";
 import {Book} from "../model/book";
 import {UserJsonService} from "../services/user-json.service";
-import {last} from "rxjs";
 import {logMessages} from "@angular-devkit/build-angular/src/tools/esbuild/utils";
 import {firebaseRepository} from "../services/firebaseRepository";
 
@@ -18,10 +17,10 @@ import {firebaseRepository} from "../services/firebaseRepository";
   styleUrl: './author-details.component.css'
 })
 export class AuthorDetailsComponent {
-  author!: author;
+  author: author | undefined;
   books: Book[] = [];
   constructor(private databaseService: UserJsonService, private firebase: firebaseRepository) { }
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
 
     // this.firebase.getAuthors()
     //   .subscribe((response: any) => {
@@ -29,19 +28,29 @@ export class AuthorDetailsComponent {
     //     console.log(response)
     //   });
 
-    this.databaseService.getAuthor()
-      .subscribe((response: any) => {
-        this.author = response;
-      });
+    // this.databaseService.getAuthor()
+    //   .subscribe((response: any) => {
+    //     this.author = response;
+    //   });
 
-    this.author.works.forEach((id) =>
-      this.databaseService.getBookById(id)
-        .subscribe((response: any) => {
-          this.books.push(response)
-        })
-    )
+    try {
+      this.author = await this.firebase.getAuthorById("iXcGKStBF26pY7gs5GCS");
 
+      if (!this.author) {
+        console.log("No se encontró ningún autor con el ID proporcionado.");
+        return;
+      }
+
+      for (const id of this.author.works) {
+        const book = await this.firebase.getBookById(id);
+        if (book) {
+          this.books.push(book);
+        } else {
+          console.log("No se encontró ningún libro con el ID proporcionado:", id);
+        }
+      }
+    } catch (error) {
+      console.error("Error al obtener detalles del autor o libros:", error);
+    }
   }
-
-  protected readonly last = last;
 }
--- Angular/src/app/services/firebaseRepository.ts
+++ Angular/src/app/services/firebaseRepository.ts
@@ -1,7 +1,8 @@
 import {inject, Injectable} from "@angular/core";
-import {collection, collectionData, Firestore} from "@angular/fire/firestore";
+import {collection, collectionData, Firestore, getDoc, DocumentData, doc, DocumentSnapshot } from "@angular/fire/firestore";
 import {Observable} from "rxjs";
 import {author} from "../model/author";
+import {Book} from "../model/book";
 
 @Injectable({
   providedIn: "root",
@@ -9,9 +10,41 @@ import {author} from "../model/author";
 
 export class firebaseRepository{
   private _firestore = inject(Firestore);
-  private _collection = collection(this._firestore,"authors")
 
   getAuthors(){
-    return collectionData(this._collection) as Observable<author[]>
+    return collectionData(collection(this._firestore,"authors")) as Observable<author[]>
+  }
+  async getAuthorById(authorId: string): Promise<author | undefined> {
+    try {
+      const docRef = doc(this._firestore, "authors", authorId);
+      const docSnap: DocumentSnapshot<any> = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        return { ...docSnap.data(), id: docSnap.id } as author;
+      } else {
+        console.log("No se encontró ningún autor con el ID proporcionado.");
+        return undefined;
+      }
+    } catch (error) {
+      console.error("Error al obtener el autor:", error);
+      return undefined;
+    }
+  }
+
+  async getBookById(bookId: string): Promise<Book | undefined> {
+    try {
+      const docRef = doc(this._firestore, "books", bookId);
+      const docSnap: DocumentSnapshot<any> = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        return { ...docSnap.data(), id: docSnap.id } as Book;
+      } else {
+        console.log("No se encontró ningún libro con el ID proporcionado.");
+        return undefined;
+      }
+    } catch (error) {
+      console.error("Error al obtener el libro:", error);
+      return undefined;
+    }
   }
 }
