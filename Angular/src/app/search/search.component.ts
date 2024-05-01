13165d7873f994227b03a344d945b1c4181777e1
sheilacazrod <sheilacazorla124@gmail.com>
2024-05-01 17:28:10 +0100

    search filterbooks and temporal searchbar

--- Angular/src/app/search/search.component.ts
+++ Angular/src/app/search/search.component.ts
@@ -2,24 +2,49 @@ import {Component, OnInit} from '@angular/core';
 import { UserJsonService } from "../services/user-json.service";
 import {Book} from "../model/book";
 import {BookComponent} from "../book/book.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [
-    BookComponent
+    BookComponent,
+    FormsModule
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent implements OnInit{
   books: Book[] = [];
+  filteredBooks: Book[] = []; // Libros filtrados
+  searchTerm: string = ''; // Término de búsqueda
 
   constructor(private databaseService: UserJsonService) { }
   ngOnInit(): void {
     this.databaseService.getBooks("assets/search.json")
       .subscribe((response: any) => {
         this.books = response.books;
+        this.filteredBooks = this.books;
       });
+  }
+
+  filterBooks(): void {
+    if (this.searchTerm.trim() === '') {
+      // Si el término de búsqueda está vacío, mostrar todos los libros
+      this.filteredBooks = this.books;
+    } else {
+      const searchTermNumber = parseFloat(this.searchTerm.trim());
+      if (!isNaN(searchTermNumber)) {
+        this.filteredBooks = this.books.filter(book =>
+          book.isbn === searchTermNumber
+        );
+      } else {
+
+        this.filteredBooks = this.books.filter(book =>
+          book.titulo.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+          book.autor.toLowerCase().includes(this.searchTerm.toLowerCase())
+        );
+      }
+    }
   }
 }
